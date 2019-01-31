fb5d9ed745d1d45acd4c58a61f36e6983efd47cd
徐鹏飞 <thomas@xupengfeideMacBook-Pro.local>
2019-01-31 10:29:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+An Exemple fot CORS.
+
+## Usage
+```bash
+# use python built http server simulated client
+# this will listen http://0.0.0.0/8000 by default
+cd fronted
+python3 -m http.server
+
+
+# use node server simulated server
+# this will listen http://localhost:8080
+cd backend
+yarn install
+node server.js
+```
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+/**
+ *  simple server for CORS test.
+ **/
+var express = require('express')
+var cors 	= require('cors')
+
+var app 	= express()
+
+app.get('/simple-s', cors(), function(req, res, next) {
+	res.json({msg: 'This is CORS-allowed.'})
+})
+
+app.get('/simple-f', function(req, res, next) {
+	res.json({msg: 'This is CORS-disallowed.'})
+})
+
+app.options('/pre-s', cors()) // enable preflight request for this request
+app.post('/pre-s', cors(), function(req, res, next) {
+	res.json({msg: 'This post request is CORS-allowed.'})
+})
+
+app.post('/pre-f', cors(), function(req, res, next) {
+	res.json({msg: 'This post request is CORS-disallowed.'})
+})
+
+app.listen(8080, function() {
+	console.log('CORS-enabled web server listening on port 8080')
+})
